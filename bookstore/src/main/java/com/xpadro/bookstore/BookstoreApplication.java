af2ef0732e65d31d7eedaecf88da01d4db74de94
Xavier Padro <xpadro.sw@gmail.com>
2020-03-23 16:39:50 +0100

    Integration with Eureka

--- bookstore/src/main/java/com/xpadro/bookstore/BookstoreApplication.java
+++ bookstore/src/main/java/com/xpadro/bookstore/BookstoreApplication.java
@@ -3,11 +3,13 @@ package com.xpadro.bookstore;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableFeignClients("com.xpadro.bookstore")
 public class BookstoreApplication {
 
