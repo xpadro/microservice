3e254c77e4ae9d224bb54051d1220a1ebae7ba7b
Xavier Padro <xpadro.sw@gmail.com>
2020-03-25 18:56:22 +0100

    First commit

---
+++ api-gateway/src/main/java/com/xpadro/apigateway/LoggingFilter.java
@@ -0,0 +1,36 @@
+package com.xpadro.apigateway;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class LoggingFilter extends ZuulFilter {
+    private static final Logger logger = LoggerFactory.getLogger(LoggingFilter.class);
+
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        HttpServletRequest request = RequestContext.getCurrentContext().getRequest();
+
+        logger.info("request: {} | request url: {}", request, request.getRequestURI());
+        return null;
+    }
+}
