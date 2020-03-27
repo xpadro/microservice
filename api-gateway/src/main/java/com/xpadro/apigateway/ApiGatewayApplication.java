542fd3dd227956ccaad2dca7c9d10d2be3082def
Xavier Padro <xpadro.sw@gmail.com>
2020-03-27 18:21:26 +0100

    Integrated logging filter

--- api-gateway/src/main/java/com/xpadro/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/xpadro/apigateway/ApiGatewayApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @EnableZuulProxy
 @EnableDiscoveryClient
@@ -14,4 +15,9 @@ public class ApiGatewayApplication {
 		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
 
+
+	@Bean
+	public LoggingFilter loggingFilter() {
+		return new LoggingFilter();
+	}
 }
